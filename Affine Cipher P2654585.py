39daa11f18413d4abaac0c2e47e7377704056ba4
P2654585 <P2654585@my365.dmu.ac.uk>
2023-01-29 14:43:29 +0000

    removed some redundant code

--- Affine Cipher P2654585.py
+++ Affine Cipher P2654585.py
@@ -12,16 +12,13 @@ import string
 def encrypt(plainText, key_a, key_b):
     
     
-    print("text") #test
     encrypted_output=[]
     arr_alphabet = list(string.ascii_uppercase) # ['a', 'b', 'c'...]
-    #print(arr_alphabet) #26 chars
     arr_plainText = list(map(lambda x: x.upper(),plainText)) #separated & converted to upper#
     length = len(arr_plainText) #length of plaintext
     for x in range (length):
-        #current = arr_plainText[x] #redundant
         affine_output = (((key_a * x) + key_b) % 26) #formula for affine cipher
-        print(x,affine_output,arr_plainText[x], ord(arr_plainText[x])) #test
+        #print(x,affine_output,arr_plainText[x], ord(arr_plainText[x])) #test
         encrypted_output.append(arr_alphabet[affine_output]) #create a new arrau from the chosen letters
         
         
@@ -34,7 +31,6 @@ def encrypt(plainText, key_a, key_b):
     arr_plainText_a = "" .join(arr_plainText)
     print("Your Input was '"+ arr_plainText_a + "'")
     print("Your generated cipher text is: ", encrypted_output)
-    #print(encrypted_output)
     main()
 
 def decrypt(cipherText, key_a, key_b):
@@ -45,7 +41,6 @@ def main():
     choice = int(input("Enter Option\n1. Encrypt\n2. Decrypt\n3. Exit the Program\nChoice: "))
 
     if choice == 1:
-        print("a")
         plainText=str(input("Enter plaintext: "))
         key_a = int(input("Enter key_a: "))
         key_b = int(input("Enter key_b: "))
