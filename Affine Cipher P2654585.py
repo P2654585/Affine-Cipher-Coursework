e89dddaeb130cdf2ba5da6357b78c485588ada04
P2654585 <P2654585@my365.dmu.ac.uk>
2023-03-09 11:37:29 +0000

    Squashed commit of the following:
    commit dd3cc4e16135f4d7fd413d2d17dc9f0866950c24
    Author: P2654585 <P2654585@my365.dmu.ac.uk>
    Date:   Thu Mar 9 11:32:58 2023 +0000
    
        Update Affine Cipher P2654585.py
    
    commit 28edd0e018c51dc8378d2cbbfaeeb6b65c9b063d
    Merge: c7e9b48 39b1f33
    Author: P2654585 <P2654585@my365.dmu.ac.uk>
    Date:   Thu Mar 9 11:32:26 2023 +0000
    
        Update README.md
    
    commit c7e9b4814889c74c8dc41f2bd198ee81d8309879
    Author: P2654585 <P2654585@my365.dmu.ac.uk>
    Date:   Thu Mar 9 11:27:52 2023 +0000
    
        Latest complete revision of program
    
        bruteforce algorithm is complete.
        allow user to raw bruteforce or sort output.
    
        heavy commented program.
    
    commit 268727fe7bcbfa0de37cdfeffae2eced3accc7e9
    Author: P2654585 <P2654585@my365.dmu.ac.uk>
    Date:   Thu Feb 23 11:49:13 2023 +0000
    
        Added Frequency analysis
    
        Added Frequency analysis to predict the probability of the answer
    
        very rough
    
    commit 48e75dc043e065f598e72c60ce13c3263d96cc75
    Author: P2654585 <P2654585@my365.dmu.ac.uk>
    Date:   Sun Feb 5 15:11:15 2023 +0000
    
        Implemented Bruteforce algorithm
    
        60-70% of coursework achieved
    
    commit 053a02cd1f2e18624b4bb18de42c679841603dc9
    Author: P2654585 <P2654585@my365.dmu.ac.uk>
    Date:   Wed Feb 1 22:16:22 2023 +0000
    
        bruteforce test 1
    
            affine_output = ((multiplicative_Inverse*(y-key_b)) % 26)
                              ~~~~~~~~~~~~~~~~~~~~~~^^~~~~~~~~
        TypeError: unsupported operand type(s) for *: 'NoneType' and 'int'

--- Affine Cipher P2654585.py
+++ Affine Cipher P2654585.py
@@ -12,11 +12,16 @@
 #3. use formula y = a^-1(y-b) % 26 <- uses euclidean algoritm
 #4. output to user
 
-import string
+#--bruteforce--
+#1. take ciphertext -> convert to upper
+#2. go throught all the combinations(12*26 = 312) ~312 tests.
+#3. will use the decrypt formula y = a^-1(y-b) % 26
+#4. output to user
+#5. rank in probabilty of correct decrypted ciphertext ? kasiski analysis/frequency analysis/entropy?
 
-#y = a * x + b (mod 26)
-#------------------------------
-#credits for this section of the code: https://www.codespeedy.com/implementation-of-affine-cipher-in-python/
+import string
+import math
+
 
 def egcd(a, b): #Extended Euclidean Algorithm
     x, y, u, v = 0, 1, 1, 0 
@@ -28,21 +33,43 @@ def egcd(a, b): #Extended Euclidean Algorithm
         b, a, x, y, u, v = a, r, u, v, m, n #update the values of b, a, x, y, u, v
     
     gcd = b #b is the GCD of a and b
-    return gcd, x, y
-
+    return gcd, x
 
 def modinv(key_a): #modular Inverse
-  gcd, x, y = egcd(key_a, 26) 
+  gcd, x = egcd(key_a, 26) 
   if gcd != 1: 
-    return None #modular inverse does not exist 
+    return 0 #modular inverse does not exist 
   else: 
     return x % 26
-#------------------------------
+
+def frequency_analysis(s): #another word for entropy
+    #Frequency Chart for English, most common letters
+    ETAOIN = { 
+        'E': 0.1202, 'T': 0.091, 'A': 0.0812, 'O': 0.0768, 'I': 0.0731,
+        'N': 0.0695, 'S': 0.0628, 'R': 0.0602, 'H': 0.0592, 'D': 0.0432, 
+        'L': 0.0398, 'U': 0.0288, 'C': .0271, 'M': 0.0261, 'F': 0.023, 
+        'Y': 0.0211, 'W': 0.0209, 'G': 0.0203, 'P': 0.0182, 'B': 0.0149, 
+        'V': 0.0111, 'K': 0.0069, 'X': 0.0017, 'Q': 0.0011, 'J': 0.001, 
+        'Z': 0.0007 
+    }
+    ascii_range = (65, 90) # A - Z
+
+    s = s.upper() #Each char case should match the dictionary keys, so it is converted to upper
+
+    #Using the frequency of a letter as p(x), calculate frequency of the string using the formula: [sum of (-log[p(x)]_2)] / len(s)
+    total_frequency = 0
+    for c in s:
+        if(ord(c) >= ascii_range[0] and ord(c) <= ascii_range[1]): # Only compute for values of A-Z
+            total_frequency += -math.log(ETAOIN[c], 2)
+
+    total_frequency = total_frequency / len(s)
+
+    return total_frequency #we only need to return the calculated value
 
 def encrypt(plainText, key_a, key_b):
     encrypted_output=[]
     arr_alphabet = list(string.ascii_uppercase) # ['A', 'B', 'C'...]
-    arr_plainText = list(map(lambda x: x.upper(),plainText)) #separated & converted to upper#
+    arr_plainText = list(map(lambda x: x.upper(),plainText)) #separated & converted to upper
     length_plainText = len(arr_plainText) #length of plaintext
     
     for x in range (length_plainText):
@@ -61,9 +88,9 @@ def decrypt(cipherText, key_a, key_b):
     try:
         decrypted_output=[] #create new array
         arr_alphabet= list(string.ascii_uppercase) #create alphabet list
-        arr_cipherText = list(map(lambda x: x.upper(),cipherText)) 
+        arr_cipherText = list(map(lambda x: x.upper(),cipherText)) #separated & converted to upper
         
-        length_cipherText = len(arr_cipherText)
+        length_cipherText = len(arr_cipherText) #length of ciphertext
         multiplicative_Inverse = modinv(key_a) #using modInv function to get multiplicitive inverse
         for x in range(length_cipherText):
             y=arr_alphabet.index(arr_cipherText[x])
@@ -83,32 +110,91 @@ def decrypt(cipherText, key_a, key_b):
         print("Error, Unable to decrypt correctly! REASON: Not able to find inverse of key_a | Returning to menu... ") #error handling
         main()
 
+def decrypt_return(cipherText, key_a, key_b): #created a decrypt return function, reused from 
+    decrypted_output = [] #create new array
+    arr_alphabet= list(string.ascii_uppercase) #create alphabet list
+    arr_cipherText = list(map(lambda x: x.upper(),cipherText)) #separated & converted to upper
+
+    length_cipherText = len(arr_cipherText) #length of ciphertext
+    multiplicative_Inverse = modinv(key_a) #using modInv function to get multiplicitive inverse
+    for x in range(length_cipherText):
+        y=arr_alphabet.index(arr_cipherText[x])
+        affine_output = ((multiplicative_Inverse*(y-key_b)) % 26)
+        #print(x,affine_output,arr_cipherText[x], ord(arr_cipherText[x])) #used for debugging
+        decrypted_output += arr_alphabet[affine_output]
+        decrypted_output = "".join(decrypted_output)
+        
+    return decrypted_output
+
+def bruteforce(cipherText): #There are only 12 possible values for key_A' #26 possible values for key_b
+    while True:
+        try: #try input
+            bruteforce_output = {} #initialise dictionary to store ciphertext with key
+            arr_cipherText = list(map(lambda x: x.upper(),cipherText)) #separated & converted to upper
+            sort_choice = 0
+            sort_choice = int(input("Enter Option\n1. all possible outputs. (Raw Bruteforce)\n2. Sorted by probablity, only 10 results with highest probablity. (Works best with ciphertext greater than 25 characters)\nChoice: "))
+
+            if sort_choice == 1:
+                for key_a in range(0,26):
+                    multiplicative_Inverse = modinv(key_a)
+                    for key_b in range(0,26):
+                        if multiplicative_Inverse != 0: #if the inverse is 1 then progress to next instruction
+                            brute = decrypt_return(cipherText,key_a,key_b)
+                            print("key_A = '"+ str(key_a) + "' Key_b = '" + str(key_b) + "' plaintext = " + brute + " Frequency Analysis: " + str(frequency_analysis(brute)))
+                            bruteforce_output[brute,'key_a:'+str(key_a), 'key_b:'+str(key_b)] = frequency_analysis(brute)#ciphertext with key
+                        #else will be caught function -> out error to user
+                    
+            elif sort_choice == 2:
+                # This happens in the background when 2 is selected.
+                for key_a in range(0,26):
+                    multiplicative_Inverse = modinv(key_a)
+                    for key_b in range(0,26):
+                        if multiplicative_Inverse != 0: #if the inverse is 1 then progress to next instruction
+                            brute = decrypt_return(cipherText,key_a,key_b)
+                            bruteforce_output[brute,'key_a:'+str(key_a), 'key_b:'+str(key_b)] = frequency_analysis(brute)#ciphertext with key
+                        #else will be caught function -> out error to user
+
+                sorted_bruteforce_output= sorted(bruteforce_output.items(), key=lambda x:x[1]) #sort items in dict in accending order, initialise as tuple
+                i = 0
+                while i < 10: #because sorted_bruteforce_output is a tuple, a loop is required to print only 10 items.
+                    if i == 0: #for the 0th output, add " <-- Highest probability answer" next to it
+                        print(str(sorted_bruteforce_output[i]) + " <-- Highest probablity answer" )
+                    else:
+                        print(sorted_bruteforce_output[i])
+                    i = i + 1 #increment by 1
+
+        except ValueError: #Error handle if not in range
+            print("Error, you did not select one of the displayed choices, try again!")
+        else:
+            main()
 
 def main(): 
     while True:
         try: #try input
 
             choice = 0 #assign
-            choice = int(input("Enter Option\n1. Encrypt\n2. Decrypt\n3. Exit the Program\nChoice: "))
+            choice = int(input("Enter Option\n1. Encrypt\n2. Decrypt\n3. Bruteforce\n4. Exit the Program\nChoice: "))
 
             if choice == 1:
                 plainText=str(input("Enter plaintext: "))
-                key_a = int(input("Enter key_a: "))
-                key_b = int(input("Enter key_b: "))
+                key_a = int(input("Enter key_a(must be a prime number): "))
+                key_b = int(input("Enter key_b(must be coprime with key_a): "))
                 encrypt(plainText, key_a, key_b)
             elif choice == 2:
                 cipherText=str(input("Enter ciphertext: "))
-                key_a = int(input("Enter key_a: "))
-                key_b = int(input("Enter key_b: "))
+                key_a = int(input("Enter key_a(must be a prime number): "))
+                key_b = int(input("Enter key_b(must be coprime with key_a): "))
                 decrypt(cipherText, key_a, key_b)
             elif choice == 3:
+                cipherText=str(input("Enter ciphertext: "))
+                bruteforce(cipherText)
+            elif choice == 4:
                 print("Exiting the program...")
                 quit()
         except ValueError: #Error handle if not in range
             print("Error, you did not select one of the displayed choices, try again!")
         else:
-            quit()
-
+            main()
 
 if __name__ == "__main__":
     main() #runs the program
--- README.md
+++ README.md
@@ -3,7 +3,14 @@
 
 Encrypt & Decrypt Fully working!
 
+This program allows for the following
+1.Encrypt Plaintext
+2. Decrypt Ciphertext
+3. Bruteforce Ciphertext, with sorting algorithm.
+
+The implementation is fully complete.
+
 credits:
 Extended Euclidean Algorithm and modular inverse - https://www.codespeedy.com/implementation-of-affine-cipher-in-python/
-
+frequency analysis: https://math.stackexchange.com/questions/3315693/using-entropy-to-determine-if-a-sentence-is-likely-to-be-valid-english
 everything else was developed from scratch.
